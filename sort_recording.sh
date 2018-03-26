e2473f157090d47d0c540a0f41a9573425bf3804
Nadia Siksnys <nsiksnys@users.noreply.github.com>
2018-03-26 13:44:34 -0300

    Update sort_recording.sh

--- sort_recording.sh
+++ sort_recording.sh
@@ -1,46 +1,68 @@
 #!/bin/bash
 
+SEARCH="Audios"
+DESTINY=$(date +%Y)
+NAMES=("")
+
 function main
 {
 	searchDir=$1
 	parentToDir=$2
-	for filename in $(ls -1 $searchDir)
+	rootDir=$PWD
+	
+	echo $rootDir
+	cd $rootDir/"$searchDir"
+	for file in $(ls -1)
 	do
-		filedate=$(echo $filename | cut -f 1 -d '_')
-		filetime=$(echo $filename | cut -f 2 -d '_' | cut -f 1 -d '-')
-
-		#if date is valid
-		if date -d "$filedate" > /dev/null 2>&1; then
-			day=$(echo $(date -d "$filedate" '+%A'))
-			
-			#just in case date +%p does not work
-			if [ $filetime -lt 12 ]; then
-			  div='am'
-			else
-			  div='pm'
-			fi
-
-			toDir=$day"_"$div
-
-			if [ -n "$parentToDir" ]; then
-				if [ ! -d "$parentToDir" ]; then
-					mkdir $parentToDir
-				fi
-			    cd $parentToDir
-			else
-				cd $searchDir
-			fi
-
-			if [ ! -d "$toDir" ]; then
-				mkdir $toDir
-			fi
-				
-			mv $filename $toDir
-			echo $filename" => "$toDir
-			cd
+		date=$(echo $file | cut -f 1 -d '_')
+		time=$(echo $file | cut -f 2 -d '_' | cut -f 1 -d '-')
+		
+		if date -d "$date" > /dev/null 2>&1; then
+			echo $file " not a valid file!"
+			continue #process next file
 		fi
+	
+		dow=$(echo $(date -d "$date" +%u)) #day of week (1: monday - 7: sunday)
+		if [[ ${NAMES[$dow]+yes} == 'yes' ]]; then #valid name for given day of week in array
+			dir=${NAMES[$dow]}
+		else
+			if date -d "$date" > /dev/null 2>&1; then #valid date
+				day=$(echo $(date -d "$date" '+%A'))
+				
+				if [ $time -lt 12 ]; then
+				  shift='morning'
+				elif [ $time -lt 18 ]; then
+				  shift='afternoon'
+				else
+				  shift='evening'
+				fi
+
+				dir=$day"_"$shift #eg: Tuesday_evening
+			fi
+		fi
+		
+		if [[ ${dir:-no} == 'no' ]]; then #no folder name set
+			echo "No folder name set!"
+			continue #process next file
+		fi
+
+		#destiny folder (files will be moved here)
+		if [ -n "$parentToDir" ]; then #if there is a parent destiny folder set
+			if [ ! -d $rootDir/"$parentToDir" ]; then #if the folder doesn't exist
+				mkdir $rootDir/"$parentToDir" #create it
+			fi
+			cd $rootDir/"$parentToDir" #change to parent destiny folder
+		fi
+
+		if [ ! -d "$dir" ]; then #if destiny folder doesn't exist
+			mkdir "$dir" #create it
+		fi
+			
+		mv $rootDir/$searchDir/$file "$dir"
+		echo $rootDir/$searchDir/$file" => "$dir
+		cd
 	done
 }
 
-main $1 $2
+main ${1:-$SEARCH} ${2:-$SEARCH}
 exit
